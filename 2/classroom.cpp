622ec058e702bbf022406ff0e6b96968d2a6be17
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2025-03-12 00:54:05 +0100

    2: typo

--- 2/classroom.cpp
+++ 2/classroom.cpp
@@ -6,6 +6,6 @@ auto Classroom::print_students_with_scholarship() const -> void {
         auto result = student.get_grades_avg();
 
         if (result.can_get_scholarship && result.value < 1.7)
-            std::cout << student.first_name + " " + student.last_name + "\t";
+            std::cout << student.first_name + " " + student.last_name + "\n";
     }
 }
